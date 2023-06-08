ae1c2a9d5e55ca710da8e4e7182180a0b04f68a1
Axel Funes <chakyfunes@gmail.com>
2023-06-08 18:12:43 -0300

    fullRow agregado

--- FiltroForm.Designer.cs
+++ FiltroForm.Designer.cs
@@ -59,9 +59,10 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(20, 162);
+            this.label1.Location = new System.Drawing.Point(15, 132);
+            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(122, 20);
+            this.label1.Size = new System.Drawing.Size(103, 17);
             this.label1.TabIndex = 0;
             this.label1.Text = "Fecha de inicio";
             // 
@@ -69,9 +70,10 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(20, 212);
+            this.label2.Location = new System.Drawing.Point(15, 172);
+            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(101, 20);
+            this.label2.Size = new System.Drawing.Size(86, 17);
             this.label2.TabIndex = 1;
             this.label2.Text = "Fecha de fin";
             // 
@@ -80,9 +82,10 @@
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(0)))));
-            this.label3.Location = new System.Drawing.Point(521, 39);
+            this.label3.Location = new System.Drawing.Point(391, 32);
+            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(252, 29);
+            this.label3.Size = new System.Drawing.Size(201, 25);
             this.label3.TabIndex = 2;
             this.label3.Text = "Consultar Encuesta";
             // 
@@ -90,10 +93,10 @@
             // 
             this.btnBuscarEncuestas.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
             this.btnBuscarEncuestas.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
-            this.btnBuscarEncuestas.Location = new System.Drawing.Point(139, 273);
-            this.btnBuscarEncuestas.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnBuscarEncuestas.Location = new System.Drawing.Point(104, 222);
+            this.btnBuscarEncuestas.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnBuscarEncuestas.Name = "btnBuscarEncuestas";
-            this.btnBuscarEncuestas.Size = new System.Drawing.Size(157, 36);
+            this.btnBuscarEncuestas.Size = new System.Drawing.Size(118, 29);
             this.btnBuscarEncuestas.TabIndex = 3;
             this.btnBuscarEncuestas.Text = "Buscar llamadas";
             this.btnBuscarEncuestas.UseVisualStyleBackColor = false;
@@ -101,19 +104,19 @@
             // 
             // dateTimeInicio
             // 
-            this.dateTimeInicio.Location = new System.Drawing.Point(163, 162);
-            this.dateTimeInicio.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimeInicio.Location = new System.Drawing.Point(122, 132);
+            this.dateTimeInicio.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimeInicio.Name = "dateTimeInicio";
-            this.dateTimeInicio.Size = new System.Drawing.Size(259, 22);
+            this.dateTimeInicio.Size = new System.Drawing.Size(195, 20);
             this.dateTimeInicio.TabIndex = 6;
             this.dateTimeInicio.Visible = false;
             // 
             // dateTimeFin
             // 
-            this.dateTimeFin.Location = new System.Drawing.Point(163, 212);
-            this.dateTimeFin.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.dateTimeFin.Location = new System.Drawing.Point(122, 172);
+            this.dateTimeFin.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.dateTimeFin.Name = "dateTimeFin";
-            this.dateTimeFin.Size = new System.Drawing.Size(259, 22);
+            this.dateTimeFin.Size = new System.Drawing.Size(195, 20);
             this.dateTimeFin.TabIndex = 7;
             this.dateTimeFin.Visible = false;
             // 
@@ -128,13 +131,15 @@
             this.EstadoActual,
             this.DuracionLlamada,
             this.DescripcionEncuesta});
-            this.grdLlamada2.Location = new System.Drawing.Point(528, 162);
-            this.grdLlamada2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.grdLlamada2.Location = new System.Drawing.Point(396, 132);
+            this.grdLlamada2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grdLlamada2.MultiSelect = false;
             this.grdLlamada2.Name = "grdLlamada2";
             this.grdLlamada2.ReadOnly = true;
             this.grdLlamada2.RowHeadersWidth = 51;
             this.grdLlamada2.RowTemplate.Height = 24;
-            this.grdLlamada2.Size = new System.Drawing.Size(847, 213);
+            this.grdLlamada2.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.grdLlamada2.Size = new System.Drawing.Size(635, 173);
             this.grdLlamada2.TabIndex = 9;
             this.grdLlamada2.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grdLlamada2_CellClick);
             // 
@@ -185,9 +190,10 @@
             this.label4.BackColor = System.Drawing.Color.Cornsilk;
             this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.Black;
-            this.label4.Location = new System.Drawing.Point(524, 117);
+            this.label4.Location = new System.Drawing.Point(393, 95);
+            this.label4.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(208, 20);
+            this.label4.Size = new System.Drawing.Size(174, 16);
             this.label4.TabIndex = 13;
             this.label4.Text = "Seleccione una llamada";
             this.label4.Visible = false;
@@ -195,10 +201,10 @@
             // btnCancelar
             // 
             this.btnCancelar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.btnCancelar.Location = new System.Drawing.Point(251, 655);
-            this.btnCancelar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnCancelar.Location = new System.Drawing.Point(188, 532);
+            this.btnCancelar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnCancelar.Name = "btnCancelar";
-            this.btnCancelar.Size = new System.Drawing.Size(107, 33);
+            this.btnCancelar.Size = new System.Drawing.Size(80, 27);
             this.btnCancelar.TabIndex = 14;
             this.btnCancelar.Text = "Cancelar";
             this.btnCancelar.UseVisualStyleBackColor = false;
@@ -212,13 +218,13 @@
             this.grdPregYRes.Columns.AddRange(new System.Windows.Forms.DataGridViewColumn[] {
             this.rtaSelec,
             this.descPreg});
-            this.grdPregYRes.Location = new System.Drawing.Point(528, 437);
-            this.grdPregYRes.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.grdPregYRes.Location = new System.Drawing.Point(396, 355);
+            this.grdPregYRes.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.grdPregYRes.Name = "grdPregYRes";
             this.grdPregYRes.ReadOnly = true;
             this.grdPregYRes.RowHeadersWidth = 51;
             this.grdPregYRes.RowTemplate.Height = 24;
-            this.grdPregYRes.Size = new System.Drawing.Size(847, 197);
+            this.grdPregYRes.Size = new System.Drawing.Size(635, 160);
             this.grdPregYRes.TabIndex = 15;
             this.grdPregYRes.Visible = false;
             // 
@@ -244,9 +250,10 @@
             this.label5.BackColor = System.Drawing.Color.Cornsilk;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label5.ForeColor = System.Drawing.Color.Black;
-            this.label5.Location = new System.Drawing.Point(11, 117);
+            this.label5.Location = new System.Drawing.Point(8, 95);
+            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(393, 20);
+            this.label5.Size = new System.Drawing.Size(324, 16);
             this.label5.TabIndex = 16;
             this.label5.Text = "Seleccione el periodo por el cual desea filtrar";
             // 
@@ -255,11 +262,9 @@
             this.gboxSelecOpGenerar.Controls.Add(this.checkImprimir);
             this.gboxSelecOpGenerar.Controls.Add(this.checkGenerarCSV);
             this.gboxSelecOpGenerar.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Bold);
-            this.gboxSelecOpGenerar.Location = new System.Drawing.Point(24, 437);
-            this.gboxSelecOpGenerar.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.gboxSelecOpGenerar.Location = new System.Drawing.Point(18, 355);
             this.gboxSelecOpGenerar.Name = "gboxSelecOpGenerar";
-            this.gboxSelecOpGenerar.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
-            this.gboxSelecOpGenerar.Size = new System.Drawing.Size(419, 146);
+            this.gboxSelecOpGenerar.Size = new System.Drawing.Size(314, 119);
             this.gboxSelecOpGenerar.TabIndex = 18;
             this.gboxSelecOpGenerar.TabStop = false;
             this.gboxSelecOpGenerar.Text = "Seleccione forma de generacion";
@@ -269,10 +274,9 @@
             // 
             this.checkImprimir.AutoSize = true;
             this.checkImprimir.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F);
-            this.checkImprimir.Location = new System.Drawing.Point(28, 100);
-            this.checkImprimir.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkImprimir.Location = new System.Drawing.Point(21, 81);
             this.checkImprimir.Name = "checkImprimir";
-            this.checkImprimir.Size = new System.Drawing.Size(92, 24);
+            this.checkImprimir.Size = new System.Drawing.Size(76, 21);
             this.checkImprimir.TabIndex = 1;
             this.checkImprimir.Text = "Imprimir";
             this.checkImprimir.UseVisualStyleBackColor = true;
@@ -282,10 +286,9 @@
             // 
             this.checkGenerarCSV.AutoSize = true;
             this.checkGenerarCSV.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.checkGenerarCSV.Location = new System.Drawing.Point(28, 52);
-            this.checkGenerarCSV.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.checkGenerarCSV.Location = new System.Drawing.Point(21, 42);
             this.checkGenerarCSV.Name = "checkGenerarCSV";
-            this.checkGenerarCSV.Size = new System.Drawing.Size(126, 24);
+            this.checkGenerarCSV.Size = new System.Drawing.Size(101, 20);
             this.checkGenerarCSV.TabIndex = 0;
             this.checkGenerarCSV.Text = "Archivo CSV";
             this.checkGenerarCSV.UseVisualStyleBackColor = true;
@@ -294,10 +297,10 @@
             // btnConfirmar
             // 
             this.btnConfirmar.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(192)))), ((int)(((byte)(128)))));
-            this.btnConfirmar.Location = new System.Drawing.Point(91, 655);
-            this.btnConfirmar.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.btnConfirmar.Location = new System.Drawing.Point(68, 532);
+            this.btnConfirmar.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.btnConfirmar.Name = "btnConfirmar";
-            this.btnConfirmar.Size = new System.Drawing.Size(107, 33);
+            this.btnConfirmar.Size = new System.Drawing.Size(80, 27);
             this.btnConfirmar.TabIndex = 19;
             this.btnConfirmar.Text = "Confirmar";
             this.btnConfirmar.UseVisualStyleBackColor = false;
@@ -306,10 +309,10 @@
             // 
             // FiltroForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.Cornsilk;
-            this.ClientSize = new System.Drawing.Size(1421, 779);
+            this.ClientSize = new System.Drawing.Size(1066, 633);
             this.Controls.Add(this.btnConfirmar);
             this.Controls.Add(this.gboxSelecOpGenerar);
             this.Controls.Add(this.label5);
@@ -323,7 +326,7 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "FiltroForm";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Consultar Encuesta";
